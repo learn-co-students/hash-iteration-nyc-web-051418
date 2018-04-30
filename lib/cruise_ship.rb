333d3354823f9d5a4fa0bb0a820e1e786baa685b
David Zeit <dzeit216@gmail.com>
2018-04-30 22:54:52 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,13 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
-end
+  winner = "" #without this line, calling on winner in line 17 would be an error because winner hasn't been defined. And even though it has been defined in line 13, that is inside the method scope and line 17 is outside the method scope. So we need to define winner also outside the method scope in order to call it later. And we can literally set it equal to anything (not just ""), because it will be overwritten by winner = name. 
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name 
+    end 
+  end 
+
+winner 
+end 
+
